14b2aecd2acbb4a8f5826775cb0507c5de89ee37
Savvas.Stephanides <savvas.stephanides@ipo.gov.uk>
2020-11-05 09:09:37 +0000

    Add code highlighting

--- src/templates/article.js
+++ src/templates/article.js
@@ -1,10 +1,21 @@
-import React from "react"
+import React, {useEffect} from "react"
 import Layout from "../layout/general"
 import { graphql } from 'gatsby'
 import "../styles/article.scss"
 import { Helmet } from "react-helmet"
+import "../styles/code-light.css"
+
+const hljs = require("highlight.js")
 
 export default function ArticleTemplate({data}) {
+
+    useEffect(() => {
+      let codeBlocks = document.querySelectorAll("pre code")
+      codeBlocks.forEach((block) => {
+        hljs.highlightBlock(block)
+      })
+    })
+
     return <Layout>
         <Helmet>
           <title>{data.articleFile.title}</title>
